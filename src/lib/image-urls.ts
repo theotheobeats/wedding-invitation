29c2ee04ec1a15bb6e4d1ed7f4c8a6f3e4b617bf
Valentinus Theo <asakvsa.idn@gmail.com>
2025-06-04 18:32:42 +0700

    refactor: Update image URL mappings in image-urls.ts to reflect new image paths and remove outdated entries for improved accuracy and consistency.

--- src/lib/image-urls.ts
+++ src/lib/image-urls.ts
@@ -2,38 +2,29 @@
 // This file maps local image paths to their UploadThing URLs
 
 export const imageUrls = {
-  // From selected-rows(5).json
-  '/IMG_0491.JPG': 'https://s4smxmfvbu.ufs.sh/f/M87ztnPlGzdbThibf49mxsjAhVcWMKYR2npEbzBU7fv89qG4',
-  '/IMG_0427.JPG': 'https://s4smxmfvbu.ufs.sh/f/M87ztnPlGzdb6SORMH1lLCHBIuoynfra1WU4bOcJNki3SeKY',
-  '/IMG_9263.JPG': 'https://s4smxmfvbu.ufs.sh/f/M87ztnPlGzdb2FNo0ZC3pjtkRMyqlSWPYgVhLiKNsI0Je9Dx',
-  '/IMG_0448.JPG': 'https://s4smxmfvbu.ufs.sh/f/M87ztnPlGzdb9PoITsHeOalHmg4Rnyt6Whs1UXrLdiEPMK3D',
-  '/IMG_0195.JPG': 'https://s4smxmfvbu.ufs.sh/f/M87ztnPlGzdbXqf6wvgDL7tpu5Zbrw18K2ojNhVncqIzeF6S',
-  '/IMG_9823.JPG': 'https://s4smxmfvbu.ufs.sh/f/M87ztnPlGzdbTIXKZxR9mxsjAhVcWMKYR2npEbzBU7fv89qG',
-  '/IMG_9223.JPG': 'https://s4smxmfvbu.ufs.sh/f/M87ztnPlGzdbGshH14OCfFdA9jbwVolHW4nXs8pSuROYk06e',
-  '/IMG_9226.JPG': 'https://s4smxmfvbu.ufs.sh/f/M87ztnPlGzdbIQBtJjXaKL9NjAoB0Z6CYvwbuVFQWUXzxqIe',
-
-  // From selected-rows(3).json
-  '/IMG_0338.JPG': 'https://s4smxmfvbu.ufs.sh/f/M87ztnPlGzdbhXWHyLQzkVC1iSYKf8Z5RXnErvwO2aAx6mMU',
-  '/Gress-518.jpg': 'https://s4smxmfvbu.ufs.sh/f/M87ztnPlGzdbNdyPJ0t1Ur0yBwME7gRxvoZcOf6jLq9XPhDt',
-  '/IMG_9798.JPG': 'https://s4smxmfvbu.ufs.sh/f/M87ztnPlGzdbrPcgnueJMC1D4ZGeypxH9Et8vfPNuwAVXncB',
-  '/IMG_9516.JPG': 'https://s4smxmfvbu.ufs.sh/f/M87ztnPlGzdbLVL5SS3Aw08W2p3ymD7SOME1tFndlXzCor4L',
-  '/IMG_0432.JPG': 'https://s4smxmfvbu.ufs.sh/f/M87ztnPlGzdbXerHo9ugDL7tpu5Zbrw18K2ojNhVncqIzeF6',
-  '/IMG_8700.JPG': 'https://s4smxmfvbu.ufs.sh/f/M87ztnPlGzdbfjDn622cDTkzlAW62qsMLYewuH5NCUx7mFXE',
-  '/IMG_9790.JPG': 'https://s4smxmfvbu.ufs.sh/f/M87ztnPlGzdbTjPdo39mxsjAhVcWMKYR2npEbzBU7fv89qG4',
-  '/IMG_0307.JPG': 'https://s4smxmfvbu.ufs.sh/f/M87ztnPlGzdb1R0sC67wJ6qlUnQodXTDOF3CVNWcZxgpuGHY',
-  '/IMG_0234.JPG': 'https://s4smxmfvbu.ufs.sh/f/M87ztnPlGzdbPqTfVnaKYEQifNSdbP93UCpFHJz8qLxrV6kw',
-
-  // From selected-rows(4).json - NEW IMAGES
-  '/IMG_0180.JPG': 'https://s4smxmfvbu.ufs.sh/f/M87ztnPlGzdbiJmuMxAsUkPlM7tfEJseDq5VBTz2Y4SOZuLI',
-  '/IMG_0237.JPG': 'https://s4smxmfvbu.ufs.sh/f/M87ztnPlGzdbA28DXp3w6JbGqkDKSOEzM74YCo5Qa1xpRwui',
-  '/IMG_0211.JPG': 'https://s4smxmfvbu.ufs.sh/f/M87ztnPlGzdboDboFapc7b9yL1dPx0taBHikAZSOJT3fRmV4',
-  '/IMG_0321.JPG': 'https://s4smxmfvbu.ufs.sh/f/M87ztnPlGzdb6INfZe1lLCHBIuoynfra1WU4bOcJNki3SeKY',
-  '/IMG_0417.JPG': 'https://s4smxmfvbu.ufs.sh/f/M87ztnPlGzdb81liXsxMqlXk5KLSTw0ZaeyhzjRcCbFJNsf2',
-  '/IMG_0380.JPG': 'https://s4smxmfvbu.ufs.sh/f/M87ztnPlGzdbAICWhhw6JbGqkDKSOEzM74YCo5Qa1xpRwui3',
-  '/IMG_0454.JPG': 'https://s4smxmfvbu.ufs.sh/f/M87ztnPlGzdbX4ioL9gDL7tpu5Zbrw18K2ojNhVncqIzeF6S',
-  '/5bd69614-7a2d-4485-9070-a65bdc7e8a3d.jpg': 'https://s4smxmfvbu.ufs.sh/f/M87ztnPlGzdbHbdNodI5DAT96IWJOjYVLk0oudK8SCa1vEGU',
   '/IMG_0419.JPG': 'https://s4smxmfvbu.ufs.sh/f/M87ztnPlGzdbQ2NmS5Tan9Zvozd742cpPuHq6lJ3VexEwY1S',
+  '/IMG_9516.JPG': 'https://s4smxmfvbu.ufs.sh/f/M87ztnPlGzdbLVL5SS3Aw08W2p3ymD7SOME1tFndlXzCor4L',
+  '/IMG_0454.JPG': 'https://s4smxmfvbu.ufs.sh/f/M87ztnPlGzdbX4ioL9gDL7tpu5Zbrw18K2ojNhVncqIzeF6S',
+  '/IMG_0417.JPG': 'https://s4smxmfvbu.ufs.sh/f/M87ztnPlGzdb81liXsxMqlXk5KLSTw0ZaeyhzjRcCbFJNsf2',
+  '/IMG_0448.JPG': 'https://s4smxmfvbu.ufs.sh/f/M87ztnPlGzdb9PoITsHeOalHmg4Rnyt6Whs1UXrLdiEPMK3D',
+  '/IMG_9798.JPG': 'https://s4smxmfvbu.ufs.sh/f/M87ztnPlGzdbrPcgnueJMC1D4ZGeypxH9Et8vfPNuwAVXncB',
+  '/IMG_0321.JPG': 'https://s4smxmfvbu.ufs.sh/f/M87ztnPlGzdb6INfZe1lLCHBIuoynfra1WU4bOcJNki3SeKY',
+  '/IMG_0180.JPG': 'https://s4smxmfvbu.ufs.sh/f/M87ztnPlGzdbiJmuMxAsUkPlM7tfEJseDq5VBTz2Y4SOZuLI',
+  '/5bd69614-7a2d-4485-9070-a65bdc7e8a3d.jpg': 'https://s4smxmfvbu.ufs.sh/f/M87ztnPlGzdbHbdNodI5DAT96IWJOjYVLk0oudK8SCa1vEGU',
+  '/IMG_9790.JPG': 'https://s4smxmfvbu.ufs.sh/f/M87ztnPlGzdbTjPdo39mxsjAhVcWMKYR2npEbzBU7fv89qG4',
+  '/IMG_9823.JPG': 'https://s4smxmfvbu.ufs.sh/f/M87ztnPlGzdbTIXKZxR9mxsjAhVcWMKYR2npEbzBU7fv89qG',
+  '/IMG_0234.JPG': 'https://s4smxmfvbu.ufs.sh/f/M87ztnPlGzdbPqTfVnaKYEQifNSdbP93UCpFHJz8qLxrV6kw',
+  '/IMG_0307.JPG': 'https://s4smxmfvbu.ufs.sh/f/M87ztnPlGzdb1R0sC67wJ6qlUnQodXTDOF3CVNWcZxgpuGHY',
+  '/IMG_0380.JPG': 'https://s4smxmfvbu.ufs.sh/f/M87ztnPlGzdbAICWhhw6JbGqkDKSOEzM74YCo5Qa1xpRwui3',
+  '/IMG_0432.JPG': 'https://s4smxmfvbu.ufs.sh/f/M87ztnPlGzdbXerHo9ugDL7tpu5Zbrw18K2ojNhVncqIzeF6',
+  '/IMG_9263.JPG': 'https://s4smxmfvbu.ufs.sh/f/M87ztnPlGzdb2FNo0ZC3pjtkRMyqlSWPYgVhLiKNsI0Je9Dx',
+  '/IMG_0195.JPG': 'https://s4smxmfvbu.ufs.sh/f/M87ztnPlGzdbXqf6wvgDL7tpu5Zbrw18K2ojNhVncqIzeF6S',
+  '/IMG_0211.JPG': 'https://s4smxmfvbu.ufs.sh/f/M87ztnPlGzdboDboFapc7b9yL1dPx0taBHikAZSOJT3fRmV4',
+  '/IMG_0237.JPG': 'https://s4smxmfvbu.ufs.sh/f/M87ztnPlGzdbA28DXp3w6JbGqkDKSOEzM74YCo5Qa1xpRwui',
   '/IMG_0288.JPG': 'https://s4smxmfvbu.ufs.sh/f/M87ztnPlGzdbLd0Ydv3Aw08W2p3ymD7SOME1tFndlXzCor4L',
+  '/IMG_0338.JPG': 'https://s4smxmfvbu.ufs.sh/f/M87ztnPlGzdbhXWHyLQzkVC1iSYKf8Z5RXnErvwO2aAx6mMU',
+  '/IMG_0427.JPG': 'https://s4smxmfvbu.ufs.sh/f/M87ztnPlGzdb6SORMH1lLCHBIuoynfra1WU4bOcJNki3SeKY',
+  '/IMG_0491.JPG': 'https://s4smxmfvbu.ufs.sh/f/M87ztnPlGzdbThibf49mxsjAhVcWMKYR2npEbzBU7fv89qG4',
 };
 
 // Images used in other sections (to exclude from gallery)
